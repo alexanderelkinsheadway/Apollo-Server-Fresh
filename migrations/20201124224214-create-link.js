34c4be49362339feade0b36cc15f960664798445
alexanderelkinsheadway <74934300+alexanderelkinsheadway@users.noreply.github.com>
2020-11-24 14:45:13 -0800

    Added Slug to Link Link.js had name and url, now it has slug too

--- migrations/20201124224214-create-link.js
+++ migrations/20201124224214-create-link.js
@@ -14,6 +14,9 @@ module.exports = {
       url: {
         type: Sequelize.STRING
       },
+      slug: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
